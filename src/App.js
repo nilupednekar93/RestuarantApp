7c76c3e5efd82648a0ed61c3a098975ef3a82ed2
Nilam <nilam.pednekar@softlabsgroup.com>
2022-12-17 12:41:17 +0530

    all food ap done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import React from 'react';
-import {Route , Routes} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import { Header, MainContainer, CreateContainer } from './Components'
+import { useStateValues } from './Context/StateProvider';
+import { getAllFoodItems } from './utils/firebaseFunctions';
+import { actionType } from './Context/reducer';
 
 const App = () => {
+
+  const [{ foodItems }, dispatch] = useStateValues();
+
+  const fetchData = async () => {
+    await getAllFoodItems().then((data) => {
+      dispatch({
+        type: actionType.SET_FOOD_ITEMS,
+        foodItems: data,
+      })
+    })
+  };
+
+  useEffect(() => {
+    fetchData()
+  }, []);
+
   return (
-    <AnimatePresence>
-    <div className='w-screen h-auto flex flex-col bg-primary'>
-      <Header />
+    <AnimatePresence exitBeforeEnter>
+      <div className='w-screen h-auto flex flex-col bg-primary'>
+        <Header />
 
-      <main className='mt-24 p-8 w-full'>
-      <Routes>
-        <Route path = "/*" element = {<MainContainer/>}/>
-          <Route path="/createItem" element={<CreateContainer />} />
+        <main className='mt-14 md:mt-20 px-4 md md:px-16 py-4 w-full'>
+          <Routes>
+            <Route path="/*" element={<MainContainer />} />
+            <Route path="/createItem" element={<CreateContainer />} />
 
-      </Routes>
-      </main>
-    </div>
+          </Routes>
+        </main>
+      </div>
     </AnimatePresence>
   )
 }
